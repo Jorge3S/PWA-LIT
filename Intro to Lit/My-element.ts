5f8e70644cd43032b083d610b03a82bd488ade3f
Jorge3S <jorge.18070030@itsmotul.edu.mx>
2023-02-09 15:40:43 -0600

    primer paso: introduction

--- Intro to Lit/My-element.ts
+++ Intro to Lit/My-element.ts
@@ -1,1 +1,17 @@
 
+import {LitElement, html} from 'lit';
+import {customElement, property} from 'lit/decorators.js';
+
+@customElement('my-element')
+export class MyElement extends LitElement {
+  @property()
+  version = 'Complete';
+
+  render() {
+    return html`
+    <p>Welcome to the Lit tutorial!</p>
+    <p>This is the ${this.version} code.</p>
+    `;
+  }
+}
+
