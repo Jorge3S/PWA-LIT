7e446b72bc335b322e7bbafb7231a54ad1f1ddfe
Jorge3S <jorge.18070030@itsmotul.edu.mx>
2023-02-09 17:47:32 -0600

    Primer paso:Adding a reactive property

--- Reactivity/my-elements.ts
+++ Reactivity/my-elements.ts
@@ -4,7 +4,7 @@ import {customElement, property} from 'lit/decorators.js';
 @customElement('my-element')
 export class MyElement extends LitElement {
   @property()
-  result: string = '';
+  result = '';
 
   flipCoin() {
     if (Math.random() < 0.5) {
@@ -21,3 +21,4 @@ export class MyElement extends LitElement {
     `;
   }
 }
+
