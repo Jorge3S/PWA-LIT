03d59ba80612a943386e811cb47c9201f954f586
Jorge3S <jorge.18070030@itsmotul.edu.mx>
2023-02-09 16:15:04 -0600

    Cuarto paso: Declarative a event listeners

--- Intro to Lit/My-element.ts
+++ Intro to Lit/My-element.ts
@@ -1,16 +1,21 @@
-
 import {LitElement, html} from 'lit';
 import {customElement, property} from 'lit/decorators.js';
 
-@customElement('my-element')
-export class MyElement extends LitElement {
-// TODO: Add a reactive property
-@property()
-  message: string = 'Hello again.';
+@customElement('name-tag')
+export class NameTag extends LitElement {
+ @property()
+  name: string = 'Your name here';
+  ChangeName(event:Event) {
+    const input = event.target as HTMLInputElement;
+    this.name = input.value;
+  }
+  
   render() {
+    // TODO: Add declarative event listener to input.
     return html`
-      <p>${this.message}</p>
+      <p>Hello, ${this.name}</p>
+      <input @input=${this.ChangeName} placeholder = "Enter your name">
     `;
   }
-}
 
+}
