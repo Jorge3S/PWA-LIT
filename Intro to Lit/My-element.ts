df27e57b3427ba8beb80c950e89e0843bd771756
Jorge3S <jorge.18070030@itsmotul.edu.mx>
2023-02-09 16:00:07 -0600

    Segundo paso: Define a

--- Intro to Lit/My-element.ts
+++ Intro to Lit/My-element.ts
@@ -1,17 +1,13 @@
 
 import {LitElement, html} from 'lit';
-import {customElement, property} from 'lit/decorators.js';
+import {customElement} from 'lit/decorators.js';
 
-@customElement('my-element')
-export class MyElement extends LitElement {
-  @property()
-  version = 'Complete';
-
-  render() {
-    return html`
-    <p>Welcome to the Lit tutorial!</p>
-    <p>This is the ${this.version} code.</p>
-    `;
-  }
+@customElement('my-jorge')
+class MyElement extends LitElement {
+  render(){
+    return html ` 
+        <p>Hello world! From my-jorge.</p>
+        `;
 }
+
 
